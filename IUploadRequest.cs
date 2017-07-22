5130a3fe9d0e424f111c9d6898d691d6aeb57225
ccang2016 <ccang2009@163.com>
2017-07-22 13:55:35 +0800

    Add files via upload

---
+++ IUploadRequest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using CCSdk.Util;
+
+
+namespace CCSdk
+{
+  /// <summary>
+  /// 上传文件请求接口，支持同时上传多个文件
+  /// </summary>
+  public interface IUploadRequest<R> : IRequest<R> where R : ResponseBase
+  {
+    /// <summary>
+    /// 设置或获取上传的文件列表
+    /// </summary>
+    string[] File { set; get; }
+  }
+}
